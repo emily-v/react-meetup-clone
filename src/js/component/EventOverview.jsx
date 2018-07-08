4f20ea7e5c2c7822e5d88f791ca9008b8fec16e5
Emily Vong <emily.vong25@gmail.com>
2018-07-08 15:25:44 +0000

    worked on home page

--- src/js/component/EventOverview.jsx
+++ src/js/component/EventOverview.jsx
@@ -7,7 +7,21 @@ function EventOverview(props){
         <div className="container">
             <div className="card">
                 <h2 className="card-header">April 28</h2>
-                <p className="card-text">Event time and date...</p>
+                <div className="row">
+                    <div className="col-3">
+                        <p>7:00 pm</p>
+                    </div>
+                    <div className="col">
+                        <div className="row">
+                            <p><a href="#">nth Event for meetup x</a></p>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="row">
+                            <p className="text-small"><a href="#">Meetup x</a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/js/component/HomeJumbotron.jsx
+++ src/js/component/HomeJumbotron.jsx
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 
 function HomeJumbotron(props){
     return (
-        <div>
-            <div className="jumbotron text-white text-center bg-dark">
-                <div className="row">
-                    <div className="col">
-                        <h1>The Meetup Clone</h1>
-                        <p>This is a mini-project for <a href="#">4Geeks Academy</a></p>
-                        <p className="lead mb-0">Using: Bootstrap, React, React-router</p>
-                    </div>
+        <div className="jumbotron text-white text-center bg-dark p-1">
+            <div className="container-fluid p-0">
+                <div className="row d-flex justify-content-between py-3 px-4">
+                    <img src="http://via.placeholder.com/150x50" alt="4geeks" />
+                    <button className="btn btn-primary" href="#">Login</button>
+                </div>
+                <div className="row text-center mx-auto my-4 pt-3 d-block">
+                    <h1>The Meetup Clone</h1>
+                    <p className="lead">This is a mini-project for <a href="#">4Geeks Academy</a></p>
+                    <p>Using: Bootstrap, React, React-router</p>
                 </div>
             </div>
         </div>
