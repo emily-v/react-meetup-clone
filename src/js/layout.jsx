1318b2bd8817751cc55bdc5da830d7eb5700dc63
KeShaunda <parrish309@gmail.com>
2018-07-11 22:54:59 +0000

    adding meetups

--- src/js/layout.jsx
+++ src/js/layout.jsx
@@ -6,6 +6,36 @@ import {MeetupGroup} from "./views/MeetupGroup.jsx";
 import {MeetupEvent} from "./views/MeetupEvent.jsx";
 
 export default class Layout extends React.Component {
+    
+    constructor(){
+        super();
+        
+        this.state = {
+            "meetups":[
+            { 
+                id:1,
+                name:"Coding Weekends",
+                description: "Fun time coding with a community of passionate developers!",
+                listOfEvents: [10,20,30]
+            },
+            {   
+                id:2,
+                name:"Information Sessions",
+                description: "Come and learn about the fun things going on at 4 Geeks academy, learning your first bit of code. Refreshements will be served.",
+                listOfEvents: [40,50,60]
+            },
+            { 
+                id:3,
+                name:"Pitch Night",
+                description: "Come out and watch the latest crop of passionate developers present there first project. Refreshements will be served.",
+                listOfEvents: [70,80,90]
+            }
+                ],
+            "events":[],
+            "session":{},
+            "isLoading": true
+        };
+    }
 
   render() {
     return (
